dd95c3caa14ae239a7011912cc5118bed5e121ff
NicolasQueiroga <62630822+NicolasQueiroga@users.noreply.github.com>
2022-03-02 17:20:33 -0300

    `lab 2` feito

--- 2-PIO-Driver/src/main.c
+++ 2-PIO-Driver/src/main.c
@@ -1,53 +1,52 @@
 #include "asf.h"
 
-#define LED_PIO           PIOC
-#define LED_PIO_ID        ID_PIOC
-#define LED_PIO_IDX       8
-#define LED_PIO_IDX_MASK  (1 << LED_PIO_IDX)
+#define LED_PIO PIOC
+#define LED_PIO_ID ID_PIOC
+#define LED_PIO_IDX 8
+#define LED_PIO_IDX_MASK (1 << LED_PIO_IDX)
 
-#define BUT_PIO			  PIOA
-#define BUT_PIO_ID		  ID_PIOA
-#define BUT_PIO_IDX		  11
-#define BUT_PIO_IDX_MASK  (1u << BUT_PIO_IDX)
+#define BUT_PIO PIOA
+#define BUT_PIO_ID ID_PIOA
+#define BUT_PIO_IDX 11
+#define BUT_PIO_IDX_MASK (1u << BUT_PIO_IDX)
 
-#define LED1_PIO           PIOA
-#define LED1_PIO_ID        ID_PIOA
-#define LED1_PIO_IDX       0
-#define LED1_PIO_IDX_MASK  (1u << LED1_PIO_IDX)
+#define LED1_PIO PIOA
+#define LED1_PIO_ID ID_PIOA
+#define LED1_PIO_IDX 0
+#define LED1_PIO_IDX_MASK (1u << LED1_PIO_IDX)
 
-#define LED2_PIO           PIOC
-#define LED2_PIO_ID        ID_PIOC
-#define LED2_PIO_IDX       30
-#define LED2_PIO_IDX_MASK  (1u << LED2_PIO_IDX)
+#define LED2_PIO PIOC
+#define LED2_PIO_ID ID_PIOC
+#define LED2_PIO_IDX 30
+#define LED2_PIO_IDX_MASK (1u << LED2_PIO_IDX)
 
-#define LED3_PIO           PIOB
-#define LED3_PIO_ID        ID_PIOB
-#define LED3_PIO_IDX       2
-#define LED3_PIO_IDX_MASK  (1u << LED3_PIO_IDX)
+#define LED3_PIO PIOB
+#define LED3_PIO_ID ID_PIOB
+#define LED3_PIO_IDX 2
+#define LED3_PIO_IDX_MASK (1u << LED3_PIO_IDX)
 
-#define BUT1_PIO			PIOD
-#define BUT1_PIO_ID		    ID_PIOD
-#define BUT1_PIO_IDX		28
-#define BUT1_PIO_IDX_MASK   (1u << BUT1_PIO_IDX)
+#define BUT1_PIO PIOD
+#define BUT1_PIO_ID ID_PIOD
+#define BUT1_PIO_IDX 28
+#define BUT1_PIO_IDX_MASK (1u << BUT1_PIO_IDX)
 
-#define BUT2_PIO			PIOC
-#define BUT2_PIO_ID		    ID_PIOC
-#define BUT2_PIO_IDX		31
-#define BUT2_PIO_IDX_MASK   (1u << BUT2_PIO_IDX)
+#define BUT2_PIO PIOC
+#define BUT2_PIO_ID ID_PIOC
+#define BUT2_PIO_IDX 31
+#define BUT2_PIO_IDX_MASK (1u << BUT2_PIO_IDX)
 
-#define BUT3_PIO			PIOA
-#define BUT3_PIO_ID		    ID_PIOA
-#define BUT3_PIO_IDX		19
-#define BUT3_PIO_IDX_MASK   (1u << BUT3_PIO_IDX)
+#define BUT3_PIO PIOA
+#define BUT3_PIO_ID ID_PIOA
+#define BUT3_PIO_IDX 19
+#define BUT3_PIO_IDX_MASK (1u << BUT3_PIO_IDX)
 
-
-void _pio_set(Pio*, const uint32_t);
-void _pio_clear(Pio*, const uint32_t);
-void _pio_pull_up(Pio*, const uint32_t, const uint32_t);
-void _pio_set_input(Pio*, const uint32_t, const uint32_t);
+void _pio_set(Pio *, const uint32_t);
+void _pio_clear(Pio *, const uint32_t);
+void _pio_pull_up(Pio *, const uint32_t, const uint32_t);
+void _pio_set_input(Pio *, const uint32_t, const uint32_t);
+void _pio_set_output(Pio *, const uint32_t, const uint32_t, const uint32_t, const uint32_t);
 void init(void);
-void blink(Pio*, const uint32_t);
-
+void blink(Pio *, const uint32_t);
 
 void _pio_set(Pio *p_pio, const uint32_t ul_mask)
 {
@@ -69,7 +68,33 @@ void _pio_pull_up(Pio *p_pio, const uint32_t ul_mask, const uint32_t ul_pull_up_
 
 void _pio_set_input(Pio *p_pio, const uint32_t ul_mask, const uint32_t ul_attribute)
 {
-	
+	_pio_pull_up(p_pio, ul_mask, ul_attribute & PIO_PULLUP);
+
+	if (ul_attribute & (PIO_DEGLITCH | PIO_DEBOUNCE))
+		p_pio->PIO_IFER = ul_mask;
+	else
+		p_pio->PIO_IFDR = ul_mask;
+
+	p_pio->PIO_ODR = ul_mask;
+	p_pio->PIO_PER = ul_mask;
+}
+
+void _pio_set_output(Pio *p_pio, const uint32_t ul_mask, const uint32_t ul_default_level, const uint32_t ul_multidrive_enable, const uint32_t ul_pull_up_enable)
+{
+	_pio_pull_up(p_pio, ul_mask, ul_pull_up_enable);
+
+	if (ul_multidrive_enable)
+		p_pio->PIO_MDER = ul_mask;
+	else
+		p_pio->PIO_MDDR = ul_mask;
+
+	if (ul_default_level)
+		_pio_set(p_pio, ul_mask);
+	else
+		_pio_clear(p_pio, ul_mask);
+
+	p_pio->PIO_OER = ul_mask;
+	p_pio->PIO_PER = ul_mask;
 }
 
 void blink(Pio *pio, const uint32_t mask)
@@ -89,60 +114,58 @@ void init(void)
 {
 	sysclk_init();
 	WDT->WDT_MR = WDT_MR_WDDIS;
-	
+
 	pmc_enable_periph_clk(LED_PIO_ID);
 	pmc_enable_periph_clk(LED1_PIO_ID);
 	pmc_enable_periph_clk(LED2_PIO_ID);
 	pmc_enable_periph_clk(LED3_PIO_ID);
-	
+
 	pmc_enable_periph_clk(BUT_PIO_ID);
 	pmc_enable_periph_clk(BUT1_PIO_ID);
 	pmc_enable_periph_clk(BUT2_PIO_ID);
 	pmc_enable_periph_clk(BUT3_PIO_ID);
 
-	pio_set_output(LED_PIO, LED_PIO_IDX_MASK, 0, 0, 0);
-	pio_set_output(LED1_PIO, LED1_PIO_IDX_MASK, 0, 0, 0);
-	pio_set_output(LED2_PIO, LED2_PIO_IDX_MASK, 0, 0, 0);
-	pio_set_output(LED3_PIO, LED3_PIO_IDX_MASK, 0, 0, 0);
-	
-	pio_set_input(BUT_PIO, BUT_PIO_IDX_MASK, PIO_DEFAULT);
-	pio_set_input(BUT1_PIO, BUT1_PIO_IDX_MASK, PIO_DEFAULT);
-	pio_set_input(BUT2_PIO, BUT2_PIO_IDX_MASK, PIO_DEFAULT);
-	pio_set_input(BUT3_PIO, BUT3_PIO_IDX_MASK, PIO_DEFAULT);
-	
+	_pio_set_output(LED_PIO, LED_PIO_IDX_MASK, 0, 0, 0);
+	_pio_set_output(LED1_PIO, LED1_PIO_IDX_MASK, 0, 0, 0);
+	_pio_set_output(LED2_PIO, LED2_PIO_IDX_MASK, 0, 0, 0);
+	_pio_set_output(LED3_PIO, LED3_PIO_IDX_MASK, 0, 0, 0);
+
+	_pio_set_input(BUT_PIO, BUT_PIO_IDX_MASK, PIO_DEFAULT);
+	_pio_set_input(BUT1_PIO, BUT1_PIO_IDX_MASK, PIO_DEFAULT);
+	_pio_set_input(BUT2_PIO, BUT2_PIO_IDX_MASK, PIO_DEFAULT);
+	_pio_set_input(BUT3_PIO, BUT3_PIO_IDX_MASK, PIO_DEFAULT);
+
 	_pio_pull_up(BUT_PIO, BUT_PIO_IDX_MASK, 1);
 	_pio_pull_up(BUT1_PIO, BUT1_PIO_IDX_MASK, 1);
 	_pio_pull_up(BUT2_PIO, BUT2_PIO_IDX_MASK, 1);
 	_pio_pull_up(BUT3_PIO, BUT3_PIO_IDX_MASK, 1);
 }
 
-
 int main(void)
 {
-  init();
-  while (1)
-  {
-	  _Bool btn = !pio_get(BUT_PIO, PIO_INPUT, BUT_PIO_IDX_MASK);
-	  _Bool btn1 = !pio_get(BUT1_PIO, PIO_INPUT, BUT1_PIO_IDX_MASK);
-	  _Bool btn2 = !pio_get(BUT2_PIO, PIO_INPUT, BUT2_PIO_IDX_MASK);
-	  _Bool btn3 = !pio_get(BUT3_PIO, PIO_INPUT, BUT3_PIO_IDX_MASK);
-	  
-	  if (btn)
-		blink(LED_PIO, LED_PIO_IDX_MASK);
-	  
-	  if (btn1)
-		blink(LED1_PIO, LED1_PIO_IDX_MASK);
-	  else if (btn2)
-		blink(LED2_PIO, LED2_PIO_IDX_MASK);
-	  else if (btn3)
-	    blink(LED3_PIO, LED3_PIO_IDX_MASK);
-	  else
-	  {
-		_pio_set(LED_PIO, LED_PIO_IDX_MASK);
-		_pio_set(LED1_PIO, LED1_PIO_IDX_MASK);
-		_pio_set(LED2_PIO, LED2_PIO_IDX_MASK);
-		_pio_set(LED3_PIO, LED3_PIO_IDX_MASK);
-	  }
-  }
-  return 0;
+	init();
+	while (1)
+	{
+		_Bool btn = !pio_get(BUT_PIO, PIO_INPUT, BUT_PIO_IDX_MASK);
+		_Bool btn1 = !pio_get(BUT1_PIO, PIO_INPUT, BUT1_PIO_IDX_MASK);
+		_Bool btn2 = !pio_get(BUT2_PIO, PIO_INPUT, BUT2_PIO_IDX_MASK);
+		_Bool btn3 = !pio_get(BUT3_PIO, PIO_INPUT, BUT3_PIO_IDX_MASK);
+
+		if (btn)
+			blink(LED_PIO, LED_PIO_IDX_MASK);
+		else if (btn1)
+			blink(LED1_PIO, LED1_PIO_IDX_MASK);
+		else if (btn2)
+			blink(LED2_PIO, LED2_PIO_IDX_MASK);
+		else if (btn3)
+			blink(LED3_PIO, LED3_PIO_IDX_MASK);
+		else
+		{
+			_pio_set(LED_PIO, LED_PIO_IDX_MASK);
+			_pio_set(LED1_PIO, LED1_PIO_IDX_MASK);
+			_pio_set(LED2_PIO, LED2_PIO_IDX_MASK);
+			_pio_set(LED3_PIO, LED3_PIO_IDX_MASK);
+		}
+	}
+	return 0;
 }
