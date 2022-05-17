9f1f5d6b829edbc11571bd045fb4739625a9a3f0
NicolasQueiroga <62630822+NicolasQueiroga@users.noreply.github.com>
2022-05-17 00:27:41 -0300

    steam img

--- 7-RTOS-LCD-LVGL/src/auxiliary/aux_lvgl/aux_lvgl.c
+++ 7-RTOS-LCD-LVGL/src/auxiliary/aux_lvgl/aux_lvgl.c
@@ -193,19 +193,10 @@ void lv_termostato(void)
 
     //
 
-    // lv_obj_t *config;
-    // create_btn(lv_scr_act(), labelConfig, set_clk_handler, &config, LV_EVENT_ALL, 60, 60);
-    // lv_obj_align_to(config, menu, LV_ALIGN_OUT_RIGHT_TOP, 0, 2);
-    // lv_obj_add_style(config, &style, 0);
-
     lv_obj_t *clk = lv_imgbtn_create(lv_scr_act());
     lv_obj_add_event_cb(clk, set_clk_handler, LV_EVENT_ALL, NULL);
 	lv_imgbtn_set_src(clk, LV_IMGBTN_STATE_RELEASED, &clock, NULL, NULL);
 	lv_obj_align_to(clk, menu, LV_ALIGN_OUT_RIGHT_MID, 0, 45);
-
-    // labelConfig = lv_label_create(config);
-    // lv_label_set_text(labelConfig, LV_SYMBOL_SETTINGS " ]");
-    // lv_obj_center(labelConfig);
 
     //
 
@@ -217,6 +208,13 @@ void lv_termostato(void)
     labelUp = lv_label_create(up);
     lv_label_set_text(labelUp, "[ " LV_SYMBOL_UP);
     lv_obj_center(labelUp);
+
+    //
+
+    lv_obj_t *steamm = lv_imgbtn_create(lv_scr_act());
+    lv_obj_add_event_cb(steamm, event_handler, LV_EVENT_ALL, NULL);
+	lv_imgbtn_set_src(steamm, LV_IMGBTN_STATE_RELEASED, &steam, NULL, NULL);
+	lv_obj_align_to(steamm, up, LV_ALIGN_OUT_RIGHT_MID, 0, -20);
 
     //
 
